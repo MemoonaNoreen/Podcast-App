08bcc1eb90f5cead30a61466dff2f84778f40b06
Developer <dev@gmail.com>
2022-01-23 21:18:09 +0500

    Last podcast link

--- app/src/main/java/com/example/driveallnight/activities/MainActivity.java
+++ app/src/main/java/com/example/driveallnight/activities/MainActivity.java
@@ -2,15 +2,61 @@ package com.example.driveallnight.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.driveallnight.R;
+import com.example.driveallnight.databinding.ActivityMainBinding;
+import com.example.driveallnight.databinding.ActivitySigninBinding;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    private static final String TAG = "MainActivity";
+    private Context context = MainActivity.this;
+
+    private ActivityMainBinding binding;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        binding.lyDrive.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        binding.lyTour.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        binding.lyShutup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        binding.lyPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                String url = "https://www.patreon.com/songsoftoriamos";
+                Uri uri = Uri.parse(url);
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
     }
 }
